818eb90a97b1bd01646881c72a1e624f7026d99d
Sergey-Draft <draftkrot1987@gmail.com>
2026-03-10 00:49:30 +0300

    add create user logic to dashboard, add usequery hooks

--- frontend/src/app/dashboard/users/page.tsx
+++ frontend/src/app/dashboard/users/page.tsx
@@ -1,10 +1,12 @@
+import CreateUser from "@/features/users/components/create-user";
 import UsersList from "@/features/users/components/users-list";
 
 export default function UsersPage() {
   return (
-    <>
+    <div className="flex jc-center gap-8">
       <div>Users page</div>
       <UsersList />
-    </>
+      <CreateUser />
+    </div>
   );
 }
